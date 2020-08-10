3b04700f8b4a5bd4094f126b5c7f5ebef6bebf8c
Lucas Maia <lucasmaiaesilva@gmail.com>
2020-08-10 19:10:51 -0300

    add filters static

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherCard from '../../components/TeacherCard';
 import styles from './styles';
 
 const Favorites = () => {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys Favoritos"/>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+      </ScrollView>
     </View>
   )
 };
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, ScrollView } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherCard from '../../components/TeacherCard';
 import styles from './styles';
 
 const TeacherList = () => {
+  const [ isFiltersVisible, setIsFiltersVisible ] = useState(false);
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(oldState => !oldState);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys Disponíveis"/>
+      <PageHeader
+        title="Proffys Disponíveis"
+        headerRight={(
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#fff" />
+          </BorderlessButton>
+        )}
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Qual a matéria?"
+              placeholderTextColor="#c1bccc"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o dia?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o horário?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      </PageHeader>
+
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{
